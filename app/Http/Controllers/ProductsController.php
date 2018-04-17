a445fb280a8d4f78d9a7a8cc577ea7b7004756ab
jamangueira7 <jamjs7@gmail.com>
2018-04-17 12:48:04 -0300

    Primeiro commit

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductsRequest as Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['user_id'] = \Auth::user()->id;
+        return Product::create($data);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function destroy(Request $request, Product $product)
+    {
+        $this->authorize('delete',$product);
+        //$product->delete();
+        return $product;
+    }
+}
---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+        return User::create($request->all());
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+        return $user;
+    }
+
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    public function destroy(Request $request, User $user)
+    {
+        $user->delete();
+        return $user;
+    }
+}
